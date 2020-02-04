57ac6c05dd4b29347e52e0a65e77b514262521db
Ahmed Elhaddad <aihaddad@outlook.com>
2020-02-04 13:42:04 +0000

    Add DB configuration

---
+++ dbexport/config.py
@@ -0,0 +1,31 @@
+import os
+
+# decorator to cache funtion results
+from functools import lru_cache
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+@lru_cache(maxsize=32)
+def engine(db_url=None):
+    db_url = db_url or os.getenv("DB_URL")
+    print(db_url)
+    if not db_url:
+        raise ValueError("Database URL is required")
+    return create_engine(db_url)
+
+
+def get_connection(db_url=None):
+    """returns a connection from an engine"""
+    return engine(db_url).connect()
+
+
+@lru_cache(maxsize=32)
+def session_class(db_url=None):
+    return sessionmaker(bind=engine(db_url))
+
+
+try:
+    Session = session_class()
+except:
+    pass
